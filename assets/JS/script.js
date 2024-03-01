46df6ac18f5fa20ac6007ebcf66abfde6a63c18f
YugendarM <yugendar2411@gmail.com>
2024-03-01 09:53:59 +0530

    Add - Js part for adding task

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,3 +1,19 @@
+const newTask = () => 
+    `<div class="to__do__task">
+        <div class="task__left__part">
+            <button class="check__button"><i class="fa-solid fa-check"></i></button>
+            <p class="task__desc">Full stack dev</p>
+        </div>
+        <button class="delete__button"><i class="fa-solid fa-trash"></i></button>
+    </div>`
+
+
+
 const handleSubmit = () => {
+    console.log("clicked");
+    const userInput = document.getElementById("input").value;
     
+    const listContainer = document.getElementById("listContainer")
+
+    listContainer.insertAdjacentHTML("beforeend",newTask())
 }
